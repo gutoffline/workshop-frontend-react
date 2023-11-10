bdf422793f58acd3042264fe7a59f39761ff703a
Guto Xavier <gutoffline@gmail.com>
2023-11-10 09:38:33 -0300

    dia 01

---
+++ workshop-frontend-react/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div class="card">
+        <h2>Rick Sanchez</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+      </div>
+    </>
+  )
+}
+
+export default App
